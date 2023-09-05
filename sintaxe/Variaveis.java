b52123eb9770c608f057bbd2b9871b579e80f947
reremeireles <renatameirelesbatista@gmail.com>
2023-09-05 19:10:40 -0300

    Feat: Declarando variáveis em Java

---
+++ sintaxe/Variaveis.java
@@ -0,0 +1,13 @@
+package sintaxe;
+
+public class Variaveis {
+  public static void main(String[] args) {
+
+    String primeiroNome = "Taylor";
+    int anoNascimento = 1989;
+    int idade = 32;
+    boolean industrialMusical = true;
+
+    idade = 33; // Atribuindo um novo valor para a variável
+  }
+}
