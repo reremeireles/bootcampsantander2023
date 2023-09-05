7e4151f61a2feb63667814532a5f85827bab02ce
reremeireles <renatameirelesbatista@gmail.com>
2023-09-05 19:08:33 -0300

    Feat: Primeira declaração de classe em Java

---
+++ sintaxe/MinhaPrimeiraClasse.java
@@ -0,0 +1,8 @@
+package sintaxe;
+
+public class MinhaPrimeiraClasse {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Oilá mundo!");
+        // Output: Oilá mundo
+    }
+}
