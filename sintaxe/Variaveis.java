442afda5e94aaa9efcfdb73e9f2c1d9cc7c89e18
reremeireles <renatameirelesbatista@gmail.com>
2023-09-07 14:26:04 -0300

    Feat: Declaração de variáveis em Java

--- sintaxe/Variaveis.java
+++ sintaxe/Variaveis.java
@@ -3,11 +3,24 @@ package sintaxe;
 public class Variaveis {
   public static void main(String[] args) {
 
-    String primeiroNome = "Taylor";
-    int anoNascimento = 1989;
-    int idade = 32;
-    boolean industrialMusical = true;
+    /* Declaração de variáveis */
 
-    idade = 33; // Atribuindo um novo valor para a variável
+    String primeiroNome = "Taylor"; // tipo: String, valor: "Taylor"
+    int anoNascimento = 1989; // tipo: int, valor: 1989
+    int idade = 32; // tipo: int, valor: 32
+    int peso; // tipo: int, valor: nenhum valor atribuído
+    boolean industrialMusical = true; // tipo: boolean, valor: true
+
+    /* Declaração com o tipo double */
+
+    double salarioMinimo = 2500; // o valor lido é 2.500
+
+    salarioMinimo = 2500.50; // o valor lido é 2.500,50
+
+    /* Variáveis são modificáveis */
+
+    int numero = 5; // atribuindo um valor para numero
+    numero = 10; // reatribuindo um valor para numero
+    System.out.print(numero); // Saída: 10
   }
 }
