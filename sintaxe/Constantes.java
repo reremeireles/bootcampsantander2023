05dac0855762388d3bd6b3fc33d7ba326b21e86c
reremeireles <renatameirelesbatista@gmail.com>
2023-09-07 14:43:51 -0300

    Feat: Declaração de constantes em Java

---
+++ sintaxe/Constantes.java
@@ -0,0 +1,12 @@
+package sintaxe;
+
+public class Constantes {
+  public static void main(String[] args) {
+
+    /* Constantes são valores não modificáveis */
+
+    final double VALOR_DE_PI = 3.14;
+    // utilizar a palavra reservada final
+    // escrever o nome da variável em caixa alta
+  }
+}
