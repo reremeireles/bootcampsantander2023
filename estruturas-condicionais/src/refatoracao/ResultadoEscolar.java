397a20a0daeb67114e2e55b857fc04892b519da0
reremeireles <renatameirelesbatista@gmail.com>
2023-09-07 18:53:13 -0300

    Feat: Refatorando código utilizando condição ternária

---
+++ estruturas-condicionais/src/refatoracao/ResultadoEscolar.java
@@ -0,0 +1,11 @@
+package refatoracao;
+
+public class ResultadoEscolar {
+  public static void main(String[] args) {
+    int nota = 7;
+
+    String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+    System.out.println(resultado);
+  }
+}
