6ac66b3755aa4d294e03c48b1ec775f2a5b95128
reremeireles <renatameirelesbatista@gmail.com>
2023-09-07 18:39:50 -0300

    Feat: Estrutura condicional simples

--- estruturas-condicionais/src/CaixaEletronico.java
+++ estruturas-condicionais/src/CaixaEletronico.java
@@ -1,15 +1,14 @@
 /* ESTRUTURA CONDICIONAL SIMPLES */
 
 public class CaixaEletronico {
-  public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-    double saldo = 25.0;
-    double valorSolicitado = 17.0;
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
 
-    if(valorSolicitado < saldo)
-      saldo = saldo - valorSolicitado;
+        if (valorSolicitado < saldo)
+            saldo = saldo - valorSolicitado;
 
-    System.out.println(saldo);
-    // 8
-  }
+        System.out.println(saldo);
+    }
 }
