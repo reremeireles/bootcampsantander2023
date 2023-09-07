b02d6ee95786c779efea480f121ec963e67cd394
reremeireles <renatameirelesbatista@gmail.com>
2023-09-07 16:38:48 -0300

    Feat: Outro exemplo de estrutura condicional composta

---
+++ estruturas-condicionais/src/Saque.java
@@ -0,0 +1,15 @@
+/* ESTRUTURA CONDICIONAL COMPOSTA */
+
+public class Saque {
+  public static void main(String[] args) {
+
+    double saldo = 25.0;
+    double valorSolicitado = 17.0;
+
+    if (valorSolicitado < saldo) {
+      saldo = saldo - valorSolicitado;
+      System.out.println("Saldo: " + saldo);
+    } else
+    System.out.println("Saldo insuficiente");
+  }
+}
