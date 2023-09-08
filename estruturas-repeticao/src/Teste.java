c54444235777f47d47e2379eb94779d2a99e3f93
reremeireles <renatameirelesbatista@gmail.com>
2023-09-08 19:29:38 -0300

    Feat: Exercícios para testar conhecimentos

---
+++ estruturas-repeticao/src/Teste.java
@@ -0,0 +1,39 @@
+/* TESTANDO CONHECIMENTOS */
+
+public class Teste {
+  public static void main(String[] args) {
+    String[] nomes = {"Camila", "Venilton", "Leonardo", "Renan", "Rafael"};
+    System.out.print(nomes.length);
+  }
+
+  // public static void main(String[] args) {
+  //       boolean condicao = false;
+
+  //       while(condicao){
+  //           System.out.println("executou ... ");
+  //       }
+  //   }
+  // public static void main(String[] args) {
+  //   int num = 5, count = 1;
+  //       do {
+  //           num += count;
+  //           System.out.println(num);
+  //       } while (count <= 3);
+  // }
+
+  // public static void main(String[] args) {
+  //       for(int x=1;x<=10;x++){
+  //           if(x % 2==0) //% operador que representa módulo de uma divisão
+  //               System.out.println(x);
+  //       }
+  //   }
+
+  // public static void main(String[] args) {
+  //       int numero = 1;
+
+  //       for (int x=1; x<2; x++){
+  //           numero = numero + x;
+  //       }
+  //       System.out.println("O valor de número é: " + numero);
+  //   }
+}
